1f6ca57538f13131e80ab5c73ae2de8c76c9daa5
Garcy111 <Garcy999@yandex.ru>
2025-03-02 10:05:44 +0300

    pc

--- src/scripts/base/examples.js
+++ src/scripts/base/examples.js
@@ -3,9 +3,6 @@ import Lenis from '@studio-freight/lenis'
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 gsap.registerPlugin(ScrollTrigger);
-import "splitting/dist/splitting.css";
-import "splitting/dist/splitting-cells.css";
-import Splitting from "splitting";
 import WOW from 'wow.js';
 
 // Compoments
@@ -15,31 +12,55 @@ import ShowBlockByTab from '../components/show-block-by-tab';
 import { slideToggle } from '../components/slide-toggle';
 import modalToggle from '../components/modal-toggle';
 import AnchorScroll from '../components/anchor-scroll';
+import isMobile from '../helpers/isMobile';
 
 document.addEventListener('DOMContentLoaded', () => {
+	new WOW().init();
+	new ItcMoveEl();
+	new modalToggle();
 
-    new WOW().init();
-    new ItcMoveEl();
-    new modalToggle();
+	// Example ShowBlockByTab
+	new ShowBlockByTab(".tabs-container");
 
-    // Example ShowBlockByTab
-    new ShowBlockByTab('.tabs-container');
+	// Example ItcCustomSelect
+	const select = new ItcCustomSelect(".custom-select");
+	document
+		.querySelector(".custom-select")
+		.addEventListener("itc.select.change", (e) => {
+			const btn = e.target.querySelector(".itc-select__toggle");
+			// выбранное значение
+			console.log(`Выбранное значение: ${btn.value}`);
+			// индекс выбранной опции
+			console.log(`Индекс выбранной опции: ${btn.dataset.index}`);
+			// выбранный текст опции
+			const selected = e.target.querySelector(".itc-select__option_selected");
+			const text = selected ? selected.textContent : "";
+			console.log(`Выбранный текст опции: ${text}`);
+		});
 
-    // Example ItcCustomSelect
-    const select = new ItcCustomSelect('.custom-select');
-    document.querySelector('.custom-select').addEventListener('itc.select.change', (e) => {
-        const btn = e.target.querySelector('.itc-select__toggle');
-        // выбранное значение
-        console.log(`Выбранное значение: ${btn.value}`);
-        // индекс выбранной опции
-        console.log(`Индекс выбранной опции: ${btn.dataset.index}`);
-        // выбранный текст опции
-        const selected = e.target.querySelector('.itc-select__option_selected');
-        const text = selected ? selected.textContent : '';
-        console.log(`Выбранный текст опции: ${text}`);
-    });
+	// AnchorScroll
+	new AnchorScroll(100);
 
-    // AnchorScroll
-    new AnchorScroll(100);
-    
+	// Lenis smooth scrolling
+	let lenis;
+
+	// Initialize Lenis smooth scrolling
+	const initSmoothScrolling = () => {
+		lenis = new Lenis({
+			lerp: 0.2,
+			smooth: isMobile() ? false : true,
+		});
+
+		lenis.on("scroll", () => ScrollTrigger.update());
+
+		const scrollFn = (time) => {
+			lenis.raf(time);
+			requestAnimationFrame(scrollFn);
+		};
+
+		requestAnimationFrame(scrollFn);
+	};
+
+	// Lenis (smooth scrolling)
+	initSmoothScrolling();
 });
